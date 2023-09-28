799dd5ff136246a86053c7ca2100b7c1d03b580b
maxwiseman <13maxwellwise@gmail.com>
2023-09-28 12:39:40 -0400

    feat(web): added hero and project section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,81 @@
-import { Button } from "ui/components/ui/button";
 import { Lenis } from "../components/lenis";
+import { ProjectCard } from "../components/project-card";
 
 export default function Page(): JSX.Element {
   return (
     <>
       <Lenis />
-      <div className="m-8 h-[200vh]">
-        <h1 className="font-bold text-4xl mb-2">The UI Library works!</h1>
-        <Button variant="default">Click me</Button>
+      <div className="bg-[#33CCFC] w-1/6 h-4/6 absolute top-[50%] left-[75%] -translate-x-1/2 -translate-y-1/2 rounded-full -rotate-45" />
+      <div className="bg-[#FE43EB] w-[10%] h-3/6 absolute top-[60%] left-[80%] -translate-x-1/2 -translate-y-1/2 rounded-full rotate-45" />
+      <div
+        className="w-screen min-h-screen backdrop-blur-[100px] z-30"
+        style={{ backgroundImage: "url('/square.png')" }}
+      >
+        <div className=" z-50 w-1/2 h-screen flex flex-col justify-center items-center px-10">
+          <div>
+            <h1 className="font-mono font-medium text-8xl w-min max-w-full">
+              Max Wiseman
+            </h1>
+            <address className="font-mono not-italic w-max text-3xl">
+              Knoxville, TN
+            </address>
+          </div>
+        </div>
+        <div className="w-screen h-screen p-24 flex flex-col justify-between items-center">
+          <h2 className="font-mono text-5xl font-medium">Projects</h2>
+          <div className="flex gap-5">
+            <ProjectCard
+              links={[
+                {
+                  type: "github",
+                  content: "https://github.com/maxwiseman/Scholarly",
+                },
+              ]}
+              title="Schoarly"
+            >
+              Labore eiusmod culpa fugiat non est ex culpa laborum laboris
+              cupidatat ad veniam. Irure nisi in quis anim deserunt fugiat ad
+              aliquip ipsum. Reprehenderit et ullamco duis minim cupidatat magna
+              occaecat id irure enim ad.
+            </ProjectCard>
+            <ProjectCard
+              links={[
+                {
+                  type: "github",
+                  content: "https://github.com/maxwiseman/personal-site",
+                },
+              ]}
+              title="Personal Site"
+            >
+              Commodo sit in quis eu mollit dolor. Excepteur ea reprehenderit
+              incididunt consequat nostrud. Magna occaecat anim duis do sit
+              consectetur aliqua. Est est id Lorem veniam consectetur culpa
+              officia duis enim cillum. Amet labore eu dolor est veniam id aute
+              deserunt est proident elit irure. Ullamco dolor ea elit ex qui.
+              Sunt magna irure culpa elit aliqua nisi ea ex ea.
+            </ProjectCard>
+            <ProjectCard
+              links={[
+                {
+                  type: "github",
+                  content: "https://github.com/maxwiseman/flashcards-v2",
+                },
+                {
+                  type: "link",
+                  content: "https://flashcards.maxwiseman.io",
+                },
+              ]}
+              title="Flashcards"
+            >
+              Culpa exercitation tempor sint aliqua sint est. Ipsum laborum
+              excepteur nostrud magna ea adipisicing reprehenderit ea nisi.
+              Eiusmod laborum minim irure voluptate cupidatat Lorem ea occaecat
+              culpa culpa. Nulla velit do fugiat irure mollit ipsum cupidatat
+              occaecat dolor eiusmod magna anim nisi fugiat. Non voluptate quis
+              ullamco in. Tempor excepteur tempor tempor ut quis.
+            </ProjectCard>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ apps/web/src/components/project-card.tsx
@@ -0,0 +1,48 @@
+import { Card, CardContent, CardTitle } from "ui/components/ui/card";
+import { Separator } from "ui/components/ui/separator";
+import { Button } from "ui/components/ui/button";
+import { GitHubLogoIcon, Link2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
+export function ProjectCard({
+  title,
+  links,
+  children,
+}: {
+  title?: string;
+  links?: {
+    type: "github" | "link";
+    content: string;
+  }[];
+  children?: React.ReactNode;
+}): JSX.Element {
+  return (
+    <Card className="min-h-max w-full max-w-max p-6">
+      {title ? (
+        <>
+          <CardTitle className="text-xl flex flex-row flex-wrap gap-2 justify-between items-center pb-1">
+            <span className="w-max">{title}</span>
+            <div className="w-max flex flex-row flex-nowrap gap-2">
+              {links?.map(link => {
+                return (
+                  <Link href={link.content} key={link.type} target="blank">
+                    <Button size="icon" variant="outline">
+                      {link.type === "github" && (
+                        <GitHubLogoIcon className="w-4 h-4" />
+                      )}
+                      {link.type === "link" && (
+                        <Link2Icon className="w-4 h-4" />
+                      )}
+                    </Button>
+                  </Link>
+                );
+              })}
+            </div>
+          </CardTitle>
+          <Separator className="my-2" />
+        </>
+      ) : null}
+      <CardContent className="p-0 pt-0">{children}</CardContent>
+    </Card>
+  );
+}
