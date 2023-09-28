9e499ef34f4af10c8871150889fdb73fffedc208
Max Wiseman <13maxwellwise@gmail.com>
2023-09-27 22:29:09 -0400

    feat(web): added smooth scrolling with lenis

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "ui/components/ui/button";
+import { Lenis } from "../components/lenis";
 
 export default function Page(): JSX.Element {
   return (
-    <div className="m-8">
-      <h1 className="font-bold text-4xl mb-2">The UI Library works!</h1>
-      <Button variant="default">Click me</Button>
-    </div>
+    <>
+      <Lenis />
+      <div className="m-8 h-[200vh]">
+        <h1 className="font-bold text-4xl mb-2">The UI Library works!</h1>
+        <Button variant="default">Click me</Button>
+      </div>
+    </>
   );
 }
---
+++ apps/web/src/components/lenis.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ReactLenis } from "@studio-freight/react-lenis";
+
+export function Lenis(): JSX.Element {
+  return <ReactLenis root />;
+}
