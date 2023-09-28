747497ecbaf748cf15442ec31539987ceacca779
Max Wiseman <13maxwellwise@gmail.com>
2023-09-27 21:42:36 -0400

    feat(ui): create ui package from shadcn/ui

--- packages/eslint-config-custom/next.js
+++ packages/eslint-config-custom/next.js
@@ -38,5 +38,7 @@ module.exports = {
   // add rules configurations here
   rules: {
     "import/no-default-export": "off",
+    // "import/no-extraneous-dependencies": ["error", {"devDependencies": false, "optionalDependencies": false, "peerDependencies": false}]
+    "import/no-extraneous-dependencies": "off"
   },
 };
