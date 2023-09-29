5dafdc65bf6c1b3e9bec3be9632662073b9d1236
maxwiseman <13maxwellwise@gmail.com>
2023-09-29 14:18:32 -0400

    feat(web): added a project details page also added next themes and vercel analytics

---
+++ apps/web/src/app/projects/[slug]/page.tsx
@@ -0,0 +1,89 @@
+import { PortableText } from "@portabletext/react";
+import { Lenis } from "../../../components/lenis";
+import client from "../../../lib/client";
+
+interface Project {
+  _createdAt: Date;
+  _updatedAt: Date;
+  _id: string;
+  _rev: string;
+  _type: "project";
+  name: string;
+  slug: { current: string; _type: "slug" };
+  description: string;
+  github: string;
+  link: string;
+  body: Block[];
+  vercel: boolean;
+  react: boolean;
+  next: boolean;
+  drizzle: boolean;
+  planetscale: boolean;
+  neon: boolean;
+  cloudflare: boolean;
+  ai: boolean;
+  tailwind: boolean;
+}
+
+interface Block {
+  _type: "block";
+  children: {
+    _type: string;
+    marks?: [];
+    text?: string;
+    _key?: string;
+    markDefs?: [];
+  }[];
+  markDefs: [];
+  style: "normal";
+}
+
+export const revalidate = 43200;
+
+export async function generateStaticParams(): Promise<string[]> {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion -- It seems that ESLint doesn't really understand this
+  const projects = (await client.fetch(`*[_type == "project"]`)) as Project[];
+  const projectSlugs = projects.map(project => {
+    return project.slug.current;
+  });
+
+  return projectSlugs;
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<JSX.Element> {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion -- It seems that ESLint doesn't really understand this
+  const projects = (await client.fetch(
+    `*[_type == "project" && slug.current == "${params.slug}"]`
+  )) as Project[];
+  const project = projects[0];
+  return (
+    <>
+      <Lenis />
+      <div className="bg-[#33CCFC] w-1/6 h-4/6 absolute top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full -rotate-45" />
+      <div className="bg-[#FE43EB] w-[10%] h-3/6 absolute top-[60%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full rotate-45" />
+      <div
+        className="w-screen min-h-screen backdrop-blur-[100px]"
+        style={{ backgroundImage: "url('/square.png')" }}
+      >
+        <div className="w-screen max-h-screen h-screen flex flex-col justify-center items-center px-10">
+          <h1 className="font-mono font-medium lg:text-8xl md:text-7xl text-6xl w-full max-w-full text-center">
+            {project.name}
+          </h1>
+          <h2 className="font-mono font-medium lg:text-5xl md:text-4xl text-3xl w-full max-w-full text-center">
+            Projects
+          </h2>
+        </div>
+
+        <div className="w-full flex justify-center">
+          <article className="max-w-3xl w-full">
+            <PortableText value={project.body} />
+          </article>
+        </div>
+      </div>
+    </>
+  );
+}
--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { TooltipProvider } from "ui/components/ui/tooltip";
+import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 export function Providers({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  return <TooltipProvider>{children}</TooltipProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Analytics />
+      <TooltipProvider>{children}</TooltipProvider>
+    </ThemeProvider>
+  );
 }
