16a476a269946d0bc32af90da9091c6e45254054
Max Wiseman <13maxwellwise@gmail.com>
2023-10-01 11:15:39 -0400

    feat(web): general styling changes

--- apps/web/src/app/projects/[slug]/code-block.tsx
+++ apps/web/src/app/projects/[slug]/code-block.tsx
@@ -1,12 +1,19 @@
 "use client";
+import { SlashIcon } from "@radix-ui/react-icons";
 import { IconClipboardCopy } from "@tabler/icons-react";
 // import "highlight.js/styles/github.css";
 import { useTheme } from "next-themes";
 import { Suspense, lazy } from "react";
 import Highlight from "react-highlight";
+import reactStringReplace from "react-string-replace";
 import { Button } from "ui/components/ui/button";
 import { Card, CardContent, CardHeader } from "ui/components/ui/card";
 import { Separator } from "ui/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "ui/components/ui/tooltip";
 import { useToast } from "ui/components/ui/use-toast";
 import { cn } from "ui/lib/utils";
 
@@ -32,7 +39,11 @@ export function CodeBlock({
       {fileName ? (
         <>
           <CardHeader className="pb-0 pt-3 flex flex-row items-center justify-between">
-            <span className="font-mono text-muted-foreground">{fileName}</span>
+            <span className="font-mono text-muted-foreground flex flex-row items-center gap-1">
+              {reactStringReplace(fileName, "/", () => {
+                return <SlashIcon className="w-4 h-4" />;
+              })}
+            </span>
           </CardHeader>
           <Separator className="my-3" />
         </>
@@ -40,32 +51,36 @@ export function CodeBlock({
         <div className="h-3" />
       )}
       <div className="relative">
-        <Button
-          className="absolute right-6 top-0 bg-card"
-          onClick={() => {
-            navigator.clipboard
-              .writeText(content || "")
-              .then(() => {
-                toast({
-                  title: "Copied to clipboard!",
-                  description:
-                    "The code was successfully copied to your clipboard",
-                  duration: 3000,
-                });
-              })
-              .catch(() => {
-                toast({
-                  variant: "destructive",
-                  title: "Error copying to clipboard",
-                  description: "We're not sure what went wrong.",
-                });
-              });
-          }}
-          size="icon"
-          variant="outline"
-        >
-          <IconClipboardCopy className="w-4 h-4" />
-        </Button>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              className="absolute right-6 top-0 bg-card"
+              onClick={() => {
+                navigator.clipboard
+                  .writeText(content || "")
+                  .then(() => {
+                    toast({
+                      // title: "Copied to clipboard!",
+                      description: "Code copied to clipboard",
+                      duration: 3000,
+                    });
+                  })
+                  .catch(() => {
+                    toast({
+                      variant: "destructive",
+                      title: "Error copying to clipboard",
+                      description: "We're not sure what went wrong.",
+                    });
+                  });
+              }}
+              size="icon"
+              variant="outline"
+            >
+              <IconClipboardCopy className="w-4 h-4" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>Copy to clipboard</TooltipContent>
+        </Tooltip>
         <CardContent className="mb-3 pb-0 flex items-center min-h-[36px] overflow-x-scroll">
           <Suspense>
             {theme === "light" ? <LightTheme /> : null}
